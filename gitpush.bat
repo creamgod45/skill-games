2637d69f32c7e82caf1d766b6211f6a39385458d
creamgod45 <fuyin1054@gmail.com>
2019-12-09 18:32:14 +0800

    update(1.0)

---
+++ gitpush.bat
@@ -0,0 +1,14 @@
+  
+@echo off
+git config --global user.email "fuyin1054@gmail.com"
+git config --global user.name "creamgod45"
+git remote add origin https://github.com/creamgod45/skill-games.git
+git add router
+git add index.php
+git add gitpush.bat
+git add .htaccess
+git add mysql
+git add docs
+git add LICENSE
+git commit -m "update(1.0)"
+git push -u origin master
