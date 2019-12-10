92da998052d824e32d42444d6c7a545a9bb36dfa
creamgod45 <fuyin1054@gmail.com>
2019-12-10 18:34:45 +0800

    creamgod45 => update(1.0)

--- router/home.php
+++ router/home.php
@@ -9,7 +9,101 @@
 </head>
 <body>
     <form class="form" action="" method="POST">
-        <title>會員註冊表單</title>
+       <div>會員註冊表單</div>
+       <table>
+           <tr>
+               <td>
+                    <div class="col name">
+                        <span>
+                            名字:
+                        </span>
+                    </div>
+                </td>
+                <td>    
+                    <div>
+                        <input type="text">
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <table>
+                <tr>
+                    <td>
+                         <div class="col username">
+                         <span>
+                            帳號:
+                         </span>
+                     </td>
+                         </div>
+                     <td>    
+                         <div>
+                             <input type="text">
+                         </div>
+                     </td>
+                 </tr>
+             </table>
+             <table>
+                    <tr>
+                        <td>
+                             <div class="col password">
+                             <span>
+                                密碼:
+                             </span>
+                         </td>
+                             </div>
+                         <td>    
+                             <div>
+                                 <input type="password">
+                             </div>
+                         </td>
+                     </tr>
+                 </table>
+        <table>
+           <tr>
+               <td>
+                    <div class="col again">
+                    <span>
+                        再次輸入密碼:
+                    </span>
+                </td>
+                    </div>
+                <td>    
+                    <div>
+                        <input type="password">
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <table>
+                <tr>
+                    <td>
+                         <div class="col email">
+                         <span>
+                            email:
+                         </span>
+                     </td>
+                         </div>
+                     <td>    
+                         <div>
+                             <input type="email">
+                         </div>
+                     </td>
+                 </tr>
+        </table>
+        <table>
+                <tr>
+                    <td>
+                        <div>
+                            <input type="submit" value="送出" style="width:40px;height:40px:font-size:20px;">
+                        </div>
+                    </td>
+                    <td>    
+                        <div>
+                            <input type="button" value="清除" style="width:40px;height:40px:font-size:20px;">
+                        </div>
+                     </td>
+                 </tr>
+        </table>
     </form>
 </body>
 </html>
