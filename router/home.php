4bfc1bef79004bd0a2c3b69bd5201ffdee4105f8
chyhhwen <chyhhwen@gmail.com>
2019-12-11 19:10:04 +0800

    chyhhwen => update(1.0)

--- router/home.php
+++ router/home.php
@@ -6,27 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./router/assets/css/main.css" >
     <title><?php echo $title;?></title>
+    <style>
+        #grad
+        {
+            width:600px;
+            height:580px;
+            background-color:red;
+            background-image: linear-gradient(rgb(100,0,0),rgb(255,0,0));
+        }  
+    </style>
 </head>
 <body>
     <form class="form" action="" method="POST">
-            <table border="1">
-  
-<div id="container" style="width:500px">
- 
-        <div id="header" style="background-color:#FFA500;">
-        <h1 style="margin-bottom:0;">主要的网页标题</h1></div>       
-        <div id="menu" style="background-color:#FFD700;height:200px;width:100px;float:left;">
-        <b>菜单</b><br>
-        HTML<br>
-        CSS<br>
-        JavaScript</div>       
-        <div id="content" style="background-color:#EEEEEE;height:200px;width:400px;float:left;">
-        内容在这里</div>
-         
-        <div id="footer" style="background-color:#FFA500;clear:both;text-align:center;">
-        版权 © runoob.com</div>
-        </div>
-         
+        <table width="300" height="200">
+            <tr>
+                <td align="center">
+                    
+                    <div id="grad" width=800px height=500px>
+                            <div><h1><font color=lightblue >照片輪播</font></h1></div>
+                            <img id="img" src="./router/assets/images/skill01.jpg" width=500px height=450px>                           
+                            <h1>美麗C世界</h1>
+                    </div>
+                </td>
+            </tr>
+        </table>
     </form>
 </body>
+<script>
+    var i = 1;
+    var opvalue = 1.0;
+    var img = document.getElementById('img');
+    setInterval(() => {
+        img.style = "opacity:"+ opvalue +";";
+        if(opvalue <0){opvalue = 1;}
+        opvalue = opvalue - 0.02;
+    }, 100);
+    setInterval(() => {
+        opvalue=1;
+        img.src = "./router/assets/images/skill0" + i + ".jpg";
+        i = i + 1;
+        if(i>2){i=1;}
+    }, 3000);
+</script>
 </html>
