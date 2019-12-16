e8eebbdca8c7d63b375cba44976cbd0b94c00863
chyhhwen <chyhhwen@gmail.com>
2019-12-16 17:22:34 +0800

    chyhhwen => update(1.0)

--- router/home.php
+++ router/home.php
@@ -8,36 +8,20 @@
     <title><?php echo $title;?></title>
 </head>
 <body style="margin: 0px;">
-    <div class ="box">
-        <div class="head">
-            <div class="name" >
-                <span style="color:brown;">WwW寫到</span>
-            </div>
-            <div class="write1">
-                <span style="color:gray;">留言主題:家除人為認知第19條</span>
-            </div>   
-            <div class="button">
-                <span><INPUT TYPE="submit" value="刪除"><INPUT TYPE="submit" value="編輯"><INPUT TYPE="submit" value="回覆"></<INPUT>
-            </div>               
-        </div>
-        <div class="head1">
-            <div class="head1" >
-                <div class="name1" style= background-color:rgb(138,136,136);>
-                    <span>留言內容</span>
-                </div>
-                <div class="write" style= background-color:rgb(168,164,164);>
-                        -----1-------------------------------------------------------------------
-                        ----1-1------------------------------------------------------------------
-                        ---1-2-1----------------------------------------------------------------
-                        --1-3-3-1----------------------------------------------------------------
-                        -1-4-6-4-1-------------------------------------------------------------
-                    </div>
-            </div>
-            
-        </div>
-    </div>
+    <div id="ex">emmmm</div>
+        <button type="submit" onclick="f()">ok</button>
+    
 </body>
 <script>
-
+    var i=0;
+    function f()
+    {
+        if(i%2==0)
+            document.getElementById("ex").innerHTML="我好可愛";
+        else
+            document.getElementById("ex").innerHTML="emmmm";
+        i+=1;
+    }
+ 
 </script>
 </html>
